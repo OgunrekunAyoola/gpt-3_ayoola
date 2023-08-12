2d823988cbd7fc6c58c8ad8fbc321595b81765bf
Ayoola Ogunrekun <ayoolaogunrekun@outlook.com>
2023-08-12 13:04:31 +0100

    next

---
+++ gpt-3/src/container/index.js
@@ -0,0 +1,6 @@
+export { default as Blog} from './Blog/Blog';
+export { default as Features} from './features/Features';
+export { default as Footer} from './Footer/footer';
+export { default as Header} from './header/header';
+export { default as Possibility} from './possibility/possibility';
+export { default as whatGPT3 } from './whatGPT-3/whatGPT3';
